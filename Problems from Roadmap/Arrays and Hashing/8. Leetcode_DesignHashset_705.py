b674d79918fefa7898a49f41a7d7bd2afb82d4a6
Paramveer Singh Gill <paramveersinghgill@Paramveers-MacBook-Air.local>
2025-03-08 00:30:29 +0530

    Added 2 problems and modified 1

--- Problems from Roadmap/Arrays and Hashing/8. Leetcode_DesignHashset_705.py
+++ Problems from Roadmap/Arrays and Hashing/8. Leetcode_DesignHashset_705.py
@@ -17,10 +17,45 @@ class MyHashSet:
         if key in self.hashset:
             return True
         return False
+    
+    #Approach 2
+    class LinkedList:
+        def __init__(self, key):
+            self.key = key
+            self.next = None
+
+    class MyHashSet:
+        def __init__(self):
+            self.hashset = [LinkedList(0) for i in range(10000)]
+
+        def add(self, key: int) -> None:
+            #Add the node with key
+            cur = self.hashset[key % len(self.hashset)]
+            while cur.next:
+                if cur.next.key == key:
+                    return
+                cur = cur.next
+            cur.next = LinkedList(key)
+
+        def remove(self, key: int) -> None:
+            cur = self.hashset[key % len(self.hashset)]
+            while cur.next:
+                if cur.next.key == key:
+                    cur.next = cur.next.next
+                    return
+                cur = cur.next
+
+        def contains(self, key: int) -> bool:
+            cur = self.hashset[key % len(self.hashset)]
+            while cur.next:
+                if cur.next.key == key:
+                    return True
+                cur = cur.next
+            return False
 
 
-# Your MyHashSet object will be instantiated and called as such:
-# obj = MyHashSet()
-# obj.add(key)
-# obj.remove(key)
-# param_3 = obj.contains(key)
+    # Your MyHashSet object will be instantiated and called as such:
+    # obj = MyHashSet()
+    # obj.add(key)
+    # obj.remove(key)
+    # param_3 = obj.contains(key)
